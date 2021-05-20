4394b976731e05ad44931fdca17580ed90eb6391
Chen22 <jingchen2222@gmail.com>
2021-05-19 20:29:04 -0700

    feat: support release zetasql package from mac os

--- pack_zetasql.sh
+++ pack_zetasql.sh
@@ -7,17 +7,27 @@ set -eE
 cd "$(dirname "$0")"
 VERSION=${TAG:-$(git rev-parse --short HEAD)}
 export ROOT=$(pwd)
-export PREFIX="$ROOT/libzetasql-$VERSION"
+export ZETASQL_LIB_NAME="libzetasql-$VERSION"
+export PREFIX="$ROOT/${ZETASQL_LIB_NAME}"
 
-rm -rf tmp-lib libzetasql.mri
+rm -rf tmp-lib libzetasql.mri ${PREFIX}
 mkdir -p tmp-lib
+mkdir -p ${PREFIX}
+mkdir -p ${PREFIX}/lib
 
 install_lib() {
     local file
     file=$1
     local libname
     libname=lib$(echo "$file" | tr '/' '_' | sed -e 's/lib//')
-    install -D "$file" "$ROOT/tmp-lib/$libname"
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]
+    then
+        INSTALL_BIN="install"
+    else
+        INSTALL_BIN="ginstall"
+    fi
+    ${INSTALL_BIN} -D "$file" "$ROOT/tmp-lib/$libname"
 }
 
 install_gen_include_file() {
@@ -25,7 +35,14 @@ install_gen_include_file() {
     file=$1
     local outfile
     outfile=$(echo "$file" | sed -e 's/^.*proto\///')
-    install -D "$file" "$PREFIX/include/$outfile"
+    
+    if [[ "$OSTYPE" == "linux-gnu"* ]]
+    then
+        INSTALL_BIN="install"
+    else
+        INSTALL_BIN="ginstall"
+    fi
+    ${INSTALL_BIN} -D "$file" "$PREFIX/include/$outfile"
 }
 
 install_external_lib() {
@@ -33,7 +50,13 @@ install_external_lib() {
     file=$1
     local libname
     libname=$(basename "$file")
-    install -D "$file" "$PREFIX/lib/$libname"
+    if [[ "$OSTYPE" == "linux-gnu"* ]]
+    then
+        INSTALL_BIN="install"
+    else
+        INSTALL_BIN="ginstall"
+    fi
+    ${INSTALL_BIN} -D "$file" "$PREFIX/lib/$libname"
 }
 
 export -f install_gen_include_file
@@ -41,41 +64,75 @@ export -f install_lib
 export -f install_external_lib
 
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+then
+    INSTALL_BIN="install"
+else
+    INSTALL_BIN="ginstall"
+fi
+
 pushd bazel-bin/
-# exlucde test so
-find zetasql -maxdepth 4 -type f -iname '*.so' -exec bash -c 'install_lib $0' {} \;
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+then
+    # exlucde test so
+    find zetasql -maxdepth 4 -type f -iname '*.so' -exec bash -c 'install_lib $0' {} \;
+fi
 find zetasql -type f -iname '*.a' -exec bash -c 'install_lib $0' {} \;
 
 # external lib headers
 pushd "$(realpath .)/../../../../../external/com_googlesource_code_re2"
-find re2 -iname "*.h" -exec install -D {} "$PREFIX"/include/{} \;
+find re2 -iname "*.h" -exec ${INSTALL_BIN} -D {} "$PREFIX"/include/{} \;
+popd
+
+pushd "$(realpath .)/../../../../../external/com_googleapis_googleapis"
+find google -iname "*.h" -exec ${INSTALL_BIN} -D {} "$PREFIX"/include/{} \;
+popd
+
+pushd "$(realpath .)/../../../../../external/com_google_file_based_test_driver"
+find file_based_test_driver -iname "*.h" -exec ${INSTALL_BIN} -D {} "$PREFIX"/include/{} \;
 popd
 
 # external lib
 pushd external
 find icu -type f -iregex ".*/.*\.\(so\|a\)\$" -exec bash -c 'install_external_lib $0' {} \;
-find com_googlesource_code_re2 -type f -iregex ".*/.*\.\(so\|a\)\$" -exec bash -c 'install_external_lib $0' {} \;
-find com_googleapis_googleapis -type f -iname '*.so' -exec bash -c 'install_external_lib $0' {} \;
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+then
+    find com_googleapis_googleapis -type f -iname '*.so' -exec bash -c 'install_external_lib $0' {} \;
+else
+    find icu -type f -iname '*.a' -exec bash -c 'install_external_lib $0' {} \;
+    find com_googlesource_code_re2 -type f -iname '*.a' -exec bash -c 'install_external_lib $0' {} \;
+    find com_googleapis_googleapis -type f -iname '*.a' -exec bash -c 'install_external_lib $0' {} \;
+    find com_google_file_based_test_driver -type f -iname '*.a' -exec bash -c 'install_external_lib $0' {} \;
+fi
 popd
 
 # zetasql generated files: protobuf & template generated files
-find zetasql -type f -iname "*.h" -exec install -D {} "$PREFIX"/include/{} \;
+find zetasql -type f -iname "*.h" -exec ${INSTALL_BIN} -D {} "$PREFIX"/include/{} \;
 find zetasql -iregex ".*/_virtual_includes/.*\.h\$" -exec bash -c 'install_gen_include_file $0' {} \;
 popd # bazel-bin
 
 # header files from source
-find zetasql -type f -iname "*.h" -exec install -D {} "$PREFIX"/include/{} \;
+find zetasql -type f -iname "*.h" -exec ${INSTALL_BIN} -D {} "$PREFIX"/include/{} \;
 
 
-echo 'create libzetasql.a' >> libzetasql.mri
-find tmp-lib/ -iname "*.a" -type f -exec bash -c 'echo "addlib $0" >> libzetasql.mri' {} \;
-echo "save" >> libzetasql.mri
-echo "end" >> libzetasql.mri
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+then
+    echo 'create libzetasql.a' >> libzetasql.mri
+    find tmp-lib/ -iname "*.a" -type f -exec bash -c 'echo "addlib $0" >> libzetasql.mri' {} \;
+    echo "save" >> libzetasql.mri
+    echo "end" >> libzetasql.mri
 
-ar -M <libzetasql.mri
-ranlib libzetasql.a
-mv libzetasql.a "$PREFIX/lib"
-mv tmp-lib/*.so "$PREFIX/lib"
-
-
-tar czf "libzetasql-$VERSION.tar.gz" "libzetasql-$VERSION"/
+    ar -M <libzetasql.mri
+    ranlib libzetasql.a
+    mv tmp-lib/*.so "$PREFIX/lib/"
+    mv libzetasql.a "$PREFIX/lib/"
+else
+    libtool -static -o libzetasql.a tmp-lib/*.a
+    mv libzetasql.a "$PREFIX/lib"
+fi
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+then
+    tar czf "${ZETASQL_LIB_NAME}-linux-x86_64.tar.gz" "${ZETASQL_LIB_NAME}"/
+else
+    tar czf "${ZETASQL_LIB_NAME}-darwin-x86_64.tar.gz" "${ZETASQL_LIB_NAME}"/
+fi
