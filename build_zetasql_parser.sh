086aaf14b00aba3ea8327e2f1d48a1740e6c0888
aceforeverd <teapot@aceforeverd.com>
2024-09-24 12:37:55 +0800

    build: impl build args in bazelrc (#58)
    * build: impl build args in bazelrc
    
    fix libstdc++ linking issue on centos7 with devtoolset
    
    * build: with 'release-static' profile
    
    update pack_zetasql_parser.sh
    
    * ci: upgrade actions version for deprecation
    
    * dbg
    
    * fix pack_zetasql.sh

--- build_zetasql_parser.sh
+++ build_zetasql_parser.sh
@@ -21,12 +21,6 @@ set -eE
 pushd "$(dirname "$0")"
 pushd "$(git rev-parse --show-toplevel)"
 
-if grep -q centos /etc/os-release ; then
-    # for thoese using rhel devtoolset
-    export BAZEL_LINKOPTS='-static-libstdc++:-lm'
-    export BAZEL_LINKLIBS='-l%:libstdc++.a'
-fi
-
 if [[ $(arch) = 'aarch64' ]]; then
     git checkout .
     # need upgrade abseil and bazel to compile on aarch64
@@ -35,21 +29,66 @@ fi
 
 echo "build with python: $(python -V), python3: $(python3 -V)"
 
+#===  FUNCTION  ================================================================
+#         NAME:  usage
+#  DESCRIPTION:  Display usage information.
+#===============================================================================
+function usage ()
+{
+    echo "Usage :  $0 [options] [--]
+
+    Options:
+    -c            Build profile. static|release|release-static, default static
+    -h|help       Display this message"
+
+}    # ----------  end of function usage  ----------
+
+#-----------------------------------------------------------------------
+#  Handle command line arguments
+#-----------------------------------------------------------------------
+
+# NOTE: default to static profile: compile all as archive but not define `NDEBUG`
+# since pre-compiled lib may used in Release/Debug profile in OpenMLDB
+#
+# acceptable values:
+# - release: `-O2 -DNDEBUG`, shared libs
+# - static:  static libs
+# - release-static: `-O2 -DNDEBUG`, static libs
+PROFILE=static
+ACCEPT_PROFILES=(static release release-static)
+
+while getopts ":hc:" opt
+do
+  case $opt in
+
+    c ) PROFILE=$OPTARG ;;
+
+    h|help     )  usage; exit 0   ;;
+
+    * )  echo -e "\n  Option does not exist : $OPTARG\n"
+          usage; exit 1   ;;
+
+  esac    # --- end of case ---
+done
+shift $((OPTIND-1))
+
+if [[ ! " ${ACCEPT_PROFILES[*]} " =~ " ${PROFILE} " ]] ; then
+    echo "profile named $PROFILE not found in accept profile list: ${ACCEPT_PROFILES[*]}"
+    exit 1
+fi
+
 TARGET='//zetasql/parser/...'
-BUILD_ARGV=(--features=-supports_dynamic_linker --sandbox_debug)
+BUILD_ARGV=(--config="$PROFILE")
 
 bazel build "$TARGET" "${BUILD_ARGV[@]}"
 bazel test "$TARGET" "${BUILD_ARGV[@]}"
 
 # explicitly build dependencies into static library
-bazel clean
+# bazel clean
 bazel query "deps(//zetasql/parser:parser)" | grep //zetasql | xargs bazel build "${BUILD_ARGV[@]}"
 bazel build "@com_googleapis_googleapis//:all" "${BUILD_ARGV[@]}"
 bazel query "@com_google_file_based_test_driver//..." | xargs bazel build "${BUILD_ARGV[@]}"
 bazel build "@com_googlesource_code_re2//:re2" "${BUILD_ARGV[@]}"
-
-unset BAZEL_LINKLIBS
-unset BAZEL_LINKOPTS
 
 popd
 popd
